35b9853e4eff7f90ece6005df0527a9e8c4ade81
yaniv.tzur@outlook.com <yaniv.tzur@outlook.com>
2018-08-25 19:56:49 +0300

    Changes
    - Further adapting baseline model to the new RT corpus.

--- baseline_model/baseline_model.py
+++ baseline_model/baseline_model.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python
 import sys
 import os
+
 from dataset_utils import dataset_utils
 from sumeval.metrics.rouge import RougeCalculator
 from textblob import TextBlob
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 import datetime
 
-USAGE_STRING = 'Usage: python baseline_model.py [corpus_path]'
+USAGE_STRING = 'Usage: python baseline_model.py [corpus_path] True/False'
+NORMALIZED_RANGE_MIN = 1
+NORMALIZED_RANGE_MAX = 5
 
-nltk.download('stopwords')
-
-english_negation_words = {'no', 'not', 'nothing', 'never', 'nowhere'}
-stopwords_to_omit = set(stopwords.words('english')).union(english_negation_words)
-ps = PorterStemmer()
 
 def train_model(train_data_set):
     """
@@ -27,48 +21,57 @@ def train_model(train_data_set):
     :return: Training set with additional required computed data
              after training.
     """
+    movie_count = 0
+    total_reviews_num = len([review for movie in train_data_set for review in movie['reviews']])
+    all_movies_sent_average = 0
+
     for movie in train_data_set:
         sentiment_score = 0
-        # Normalize the average rating given by critics to the movie
-        # from a scale of 0 to 10 to a scale of 1 to 5.
-        real_average_rating = round(float(movie['average_rating'].strip()) / 2)
-        movie['rating_label'] = real_average_rating
+        review_ratings = [shift_scale(review['rating'], 1, 5, NORMALIZED_RANGE_MIN, NORMALIZED_RANGE_MAX)
+                          for review in movie['reviews']]
+        movie['rating_label'] = sum(review_ratings) / len(review_ratings)
         for review in movie['reviews']:
             sentiment_score += get_sentiment_score(review['text'])
 
-        sentiment_score_average = round(sentiment_score / len(movie['reviews']))
-        movie['sentiScore'] = sentiment_score_average
+        sentiment_score_average = sentiment_score / len(movie['reviews'])
+        curr_movie_sentiment_average = movie['rating_label'] / sentiment_score_average
+        all_movies_sent_average += (curr_movie_sentiment_average * len(movie['reviews'])) \
+                                   / \
+                                   total_reviews_num
+        # Set the ratio between the "real" rating of the movie and the average of
+        # the sentiment scores computed for each review, averaged over the number of reviews.
+        movie_count += 1
+        print("Movies trained: " + str(movie_count))
 
-        # Set the ratio between the "real" rating of the movie and the average of the sentiment scores
-        # computed for each review, averaged over the number of reviews.
-        movie['sentiRatio'] = real_average_rating / sentiment_score_average
+    sentiment_ratio = min(all_movies_sent_average, NORMALIZED_RANGE_MAX)
+    print("{0}: Trained sentiment ratio is {1}".format(datetime.datetime.now(),
+                                                       sentiment_ratio))
 
-    return train_data_set
+    return sentiment_ratio
 
 
-def decode(test_data, trained_data):
-    sentiment_score = 0
-
-    test_to_trained_map = dict()
-
-    for test_movie in test_data:
-        for trained_movie in trained_data:
-            test_to_trained_map[test_movie] = trained_movie
+def decode(test_data, sentiment_ratio):
+    movie_count = 0
 
     for movie in test_data:
+        sentiment_score = 0
         for review in movie['reviews']:
             sentiment_score += get_sentiment_score(review['text'])
         sentiment_score_average = sentiment_score / len(movie['reviews'])
-        movie['average_rating'] = round(sentiment_score_average * test_to_trained_map[movie]['sentiRatio'])
-        # if test_data[movie]['overall'] > 5:
-        #     test_data[movie]['overall'] = 5
+
+        movie['average_rating'] = min(round(sentiment_score_average * sentiment_ratio), NORMALIZED_RANGE_MAX)
 
         # Set the first summary as the baseline summary:
         # we take the first 5 words of a random review as a summary.
         first_review_words = movie['reviews'][0]['text'].split()
         first_review_length = len(first_review_words)
         movie['summary'] = movie['reviews'][0]['text'].split()[:min(5, first_review_length)]
-        test_data[movie]['summary'] = ' '.join(test_data[movie]['summary'])
+        movie['summary'] = ' '.join(movie['summary'])
+        movie_count += 1
+        print("Movies decoded: {0}, computed rating before rounding: {1}, after rounding: {2}"
+              .format(str(movie_count),
+                      sentiment_score_average,
+                      round(sentiment_score_average * sentiment_ratio)))
 
     return test_data
 
@@ -98,7 +101,7 @@ def get_sentiment_score(text):
     :param text: the input text whose sentiment score is to be computed and returned.
     :return: the sentiment score of the input text, normalized to a discrete scale of {1,2,3,4,5}.
     """
-    return round(shift_scale(TextBlob(text).sentiment.polarity, -1, 1, 1, 5))
+    return round(shift_scale(TextBlob(text).sentiment.polarity, -1, 1, NORMALIZED_RANGE_MIN, NORMALIZED_RANGE_MAX))
 
 
 def evaluate_predicted_sentiment(decoded_test_data, gold_data):
@@ -111,16 +114,14 @@ def evaluate_predicted_sentiment(decoded_test_data, gold_data):
     accuracy_percentage_sum = 0
     for decoded_movie in decoded_test_data:
         for gold_movie in gold_data:
-            # accuracy_percentage = ((abs(gold_data[movie]['average_rating']
-            #                         -
-            #                         abs(decoded_test_data[movie]['average_rating']
-            #                         -
-            #                         gold_data[movie]['average_rating']))) / gold_data[movie]['average_rating'])\
-            #                         * \
-            #                         100
-            if decoded_movie['id'] == gold_movie['id'] \
-                    and decoded_movie['average_rating'] == gold_movie['average_rating']:
-                accuracy_percentage_sum += 1
+            if decoded_movie['id'] == gold_movie['id']:
+                review_ratings = [shift_scale(review['rating'], 1, 5, NORMALIZED_RANGE_MIN, NORMALIZED_RANGE_MAX)
+                                  for review in gold_movie['reviews']]
+                gold_movie['rating_label'] = round(sum(review_ratings) / len(review_ratings))
+                # print('test rating: {}, gold rating: {}'.format(decoded_movie['average_rating'],
+                #                                                 gold_movie['rating_label']))
+                if decoded_movie['average_rating'] == gold_movie['rating_label']:
+                    accuracy_percentage_sum += 1
         counter += 1
         accuracy_percentage = (accuracy_percentage_sum * 1.0) / counter
         eval_file.write(str(counter) + '\t\t\t' + str(accuracy_percentage) + '\n')
@@ -142,11 +143,12 @@ def evaluate_summary(decoded_test_data, gold_data, n_gram_order):
     test_to_gold_map = dict()
     for decoded_movie in decoded_test_data:
         for gold_movie in gold_data:
-            test_to_gold_map[decoded_movie] = gold_movie
+            if decoded_movie['id'] == gold_movie['id']:
+                test_to_gold_map[decoded_movie['id']] = (decoded_movie, gold_movie)
 
-    for decoded_movie in test_to_gold_map.keys():
-        rouge = calculate_rouge(test_to_gold_map[decoded_movie]["summary"],
-                                decoded_movie["summary"],
+    for decoded_movie_id in test_to_gold_map.keys():
+        rouge = calculate_rouge(test_to_gold_map[decoded_movie_id][1]["summary"],
+                                test_to_gold_map[decoded_movie_id][0]["summary"],
                                 n_gram_order)
         recall_sum += rouge['recall']
         precision_sum += rouge['precision']
@@ -168,17 +170,17 @@ def calculate_rouge(summary_gold, summary_test, ngram_order):
     rouge_recall = rouge.rouge_n(
         summary=summary_test,
         references=[summary_gold],
-        n=ngram_order, alpha=0)
+        n=ngram_order, alpha=0.5)
 
     rouge_precision = rouge.rouge_n(
         summary=summary_test,
         references=[summary_gold],
-        n=ngram_order, alpha=0)
+        n=ngram_order, alpha=0.5)
 
     rouge_f_score = rouge.rouge_n(
         summary=summary_test,
         references=[summary_gold],
-        n=ngram_order, alpha=0)
+        n=ngram_order, alpha=0.5)
     # rouge_recall = rouge.rouge_n(
     #     summary=summary_test,
     #     references=[summary_gold],
@@ -196,50 +198,35 @@ def calculate_rouge(summary_gold, summary_test, ngram_order):
     return {'recall': rouge_recall, 'precision': rouge_precision, 'fScore': rouge_f_score}
 
 
-def preprocess_text(text):
-    """
-    Preprocesses the input text and returns the preprocessed string.
-    Preprocessing includes removing numbers, converting all words to lowercase, removing stopwords and
-    stemming each remaining word.
-    :param text: the input text to preprocess.
-    :return: the preprocessed string.
-    """
-    text = re.sub('[^a-zA-Z]', ' ', text)
-    text = text.lower()
-    text = text.split()
-    text = [ps.stem(word) for word in text if word not in stopwords_to_omit]
-    text = ' '.join(text)
-    return text
-
-
-def preprocess_datasets(data_sets):
-    movie_count = 0
-    for key in data_sets.keys():
-        for movie in data_sets[key]:
-            movie['summary'] = preprocess_text(movie['summary'])
-            for review in movie['reviews']:
-                review['text'] = preprocess_text(review['text'])
-        movie_count += 1
-        print(movie_count)
-    return data_sets
-
-
 def main():
     # Usage of the program is of the form 'python baseline_model.py [dataset_path]'.
-    if (len(sys.argv) > 2) or not os.path.exists(sys.argv[1]):
+    if len(sys.argv) > 3:
+        print(USAGE_STRING)
+        exit(1)
+    if not os.path.exists(sys.argv[1]):
+        print('File with path {} does not exist.'.format(sys.argv[1]))
+        exit(1)
+    if sys.argv[2].lower() not in ['true', 'false']:
         print(USAGE_STRING)
         exit(1)
 
-    data_sets = dataset_utils.build_data_sets_from_json_file(sys.argv[1])
-    print("{} : Starting preprocessing".format(datetime.datetime.now()))
-    data_sets = preprocess_datasets(data_sets)
-    print("{} : End of preprocessing".format(datetime.datetime.now()))
-    trained_data = train_model(data_sets['train'])
-    decoded_test_data = decode(data_sets['test'], trained_data)
+    if sys.argv[2].lower() == 'true':
+        preprocess = True
+    else:
+        preprocess = False
+    data_sets = dataset_utils.build_data_sets_from_json_file(sys.argv[1], preprocess)
+    print("{} : Starting training".format(datetime.datetime.now()))
+    trained_sentiment_ratio = train_model(data_sets['train'])
+    print("{} : End of training".format(datetime.datetime.now()))
+    print("{} : Starting decoding".format(datetime.datetime.now()))
+    decoded_test_data = decode(data_sets['test'], trained_sentiment_ratio)
+    print("{} : End of decoding".format(datetime.datetime.now()))
     gold_data = prepare_gold_data(data_sets['gold'])
+    print("{} : Starting evaluation".format(datetime.datetime.now()))
     evaluate_predicted_sentiment(decoded_test_data, gold_data)
     evaluate_summary(decoded_test_data, gold_data, 1)
     evaluate_summary(decoded_test_data, gold_data, 2)
+    print("{} : End of evaluation".format(datetime.datetime.now()))
 
 
 main()
--- dataset_utils/dataset_utils.py
+++ dataset_utils/dataset_utils.py
@@ -1,7 +1,25 @@
 import json
-
+import os
+import datetime
+import re
 from pandas.io.json import json_normalize
 from sklearn.model_selection import train_test_split
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+BASE_PREPROCESSED_PATH = r"C:\Users\yaniv\Dropbox\Second Degree - OU\12. Introduction to Natural Language " \
+                         + \
+                         r"Processing\project\shared\\"
+PREPROCESSED_TRAIN_PATH = BASE_PREPROCESSED_PATH + 'train_preprocessed.json'
+PREPROCESSED_TEST_PATH = BASE_PREPROCESSED_PATH + 'test_preprocessed.json'
+PREPROCESSED_GOLD_PATH = BASE_PREPROCESSED_PATH + 'gold_preprocessed.json'
+
+nltk.download('stopwords')
+
+english_negation_words = {'no', 'not', 'nothing', 'never', 'nowhere', 'none', 'no one', 'nobody'}
+stopwords_to_omit = set(stopwords.words('english')).union(english_negation_words)
+ps = PorterStemmer()
 
 
 def build_corpus(json_data_dir):
@@ -10,7 +28,40 @@ def build_corpus(json_data_dir):
     return corpus
 
 
-def build_data_sets_from_corpus(corpus):
+def preprocess_text(text):
+    """
+    Preprocesses the input text and returns the preprocessed string.
+    Preprocessing includes removing numbers, converting all words to lowercase, removing stopwords and
+    stemming each remaining word.
+    :param text: the input text to preprocess.
+    :return: the preprocessed string.
+    """
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    text = text.lower()
+    text = text.split()
+    text = [ps.stem(word) for word in text if word not in stopwords_to_omit]
+    text = ' '.join(text)
+    return text
+
+
+def preprocess_datasets(data_sets):
+    print('{}: Starting to preprocess datasets'.format(datetime.datetime.now()))
+    movie_count = 0
+    for key in data_sets.keys():
+        for movie in data_sets[key]:
+            if 'summary' in movie.keys():
+                movie['summary'] = preprocess_text(movie['summary'])
+            for review in movie['reviews']:
+                review['text'] = preprocess_text(review['text'])
+            movie_count += 1
+            print("Movies preprocessed: {}".format(movie_count))
+        with open(BASE_PREPROCESSED_PATH + '{}_preprocessed.json'.format(key), 'w') as output_file:
+            json.dump(data_sets[key], output_file)
+    print('{}: End of preprocessing'.format(datetime.datetime.now()))
+    return data_sets
+
+
+def build_data_sets_from_corpus(corpus, preprocess=False):
     data_sets = {'train': [], 'gold': [], 'test': []}
 
     complete_dataset = json_normalize(corpus['movies'])
@@ -24,10 +75,24 @@ def build_data_sets_from_corpus(corpus):
     data_sets['test'] = gold_set.drop('summary', axis=1)\
                                 .drop('average_rating', axis=1)\
                                 .to_dict('records')
-
+    if preprocess:
+        data_sets = preprocess_datasets(data_sets)
     return data_sets
 
 
-def build_data_sets_from_json_file(json_file_path):
+def preprocessed_data_sets_exist():
+    return os.path.exists(PREPROCESSED_TRAIN_PATH) and os.path.exists(PREPROCESSED_TEST_PATH)\
+           and os.path.exists(PREPROCESSED_GOLD_PATH)
+
+
+def get_preprocessed_data_sets():
+    return {'train': json.load(open(PREPROCESSED_TRAIN_PATH, 'r')),
+            'test': json.load(open(PREPROCESSED_TEST_PATH, 'r')),
+            'gold': json.load(open(PREPROCESSED_GOLD_PATH, 'r'))}
+
+
+def build_data_sets_from_json_file(json_file_path, preprocess=False):
+    if preprocess and preprocessed_data_sets_exist():
+        return get_preprocessed_data_sets()
     corpus = build_corpus(json_file_path)
-    return build_data_sets_from_corpus(corpus)
+    return build_data_sets_from_corpus(corpus, preprocess)
